68eadd7801fc9e346b230c8ce651939271677e2b
Ottoniel Domínguez González <otto@ottodomm.com>
2018-08-16 00:18:26 -0500

    Add files via upload

---
+++ src/main/java/com/rato/boot/mp/inicial/InicialApplication.java
@@ -0,0 +1,12 @@
+package com.rato.boot.mp.inicial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InicialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InicialApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rato/boot/mp/inicial/InicialApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rato.boot.mp.inicial;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InicialApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
